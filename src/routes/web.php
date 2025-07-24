d95304757165134b80ff69b70ecde8ca1c57b884
maruyu <goldenleaf3310@gmail.com>
2025-07-24 11:05:38 +0900

    Fortify導入と設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/weight/create', function () {
+    return view('weights.create');
+});
